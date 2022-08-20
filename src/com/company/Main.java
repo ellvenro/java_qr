cffbfe7d852d12d65a13ad79f29353879be5231d
ellvenro <lidiya.ellie@yandex.ru>
2022-08-20 17:15:34 +0300

    Correct

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input: ");
+        String str = in.nextLine();
+        System.out.printf("Output: %s\n", str);
+        in.close();
+        coding(str);
+    }
+
+    public static void coding (String str) {
+        int lenMas = (int)Math.ceil((float)str.length() / 3);
+        int lenStr = str.length();
+        char[][] code = new char[lenMas][3];
+
+        for (int i = 0, j = 0; i < lenStr; i+=3, j++) {
+            str.getChars(i, Math.min(i + 3, lenStr), code[j], 0);
+        }
+        for (int i = 0; i < lenMas; i++)
+            System.out.println(code[i]);
     }
 }
