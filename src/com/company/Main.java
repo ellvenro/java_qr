de5367ceb26f66a2e9d629645e219a9b6554aad7
ellvenro <lidiya.ellie@yandex.ru>
2022-08-20 19:30:00 +0300

    Filling with alternating bytes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,11 +11,11 @@ public class Main {
         System.out.printf("Output: %s\n", str);
         in.close();
         coding(str);
-        addServiceInformation(str);
     }
 
-    //Кодировка исходной строки в UTF-8
-    public static String coding (String str) {
+    //Создание кода
+    public static void coding (String str) {
+        //Кодировка исходной строки в UTF-8
         byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
         String bin = "";
 
@@ -23,9 +23,13 @@ public class Main {
             bin = String.join("", bin, String.format("%8s", Integer.toBinaryString(aByte)).replace(' ', '0'));
         }
 
-        //Дополнение строки нулями для кратности восьми
-        bin = String.join("", bin, "0000");
-        return bin;
+        //Дополнение строки нулями для кратности восьми и сервисной информацией
+        bin = String.join("", addServiceInformation(str), bin, "0000");
+
+        //Добавление чередующихся байтов
+        bin = Filling(str, bin);
+
+        System.out.print(bin);
     }
 
     //Первая версия qr, побайтовое кодирование
@@ -37,11 +41,25 @@ public class Main {
 
     //Определение максимального количества информации и уровня коррекции
     public static int determLevel (String str) {
-        int len = str.length();
+        int len = str.length() * 8 + 4 + 8;
         if (len <= 72) return 72;        // H
         else if (len <= 104) return 104; // Q
         else if (len <= 128) return 128; // M
         else if (len <= 152) return 152; // L
         else return -1;                  //Не подходит по условиям, ошибка
+    }
+
+    //Заполнение информационного блока пустыми байтами
+    public static String Filling (String str, String bin) {
+        int cntInf = determLevel(str);
+        int sub = cntInf - bin.length();
+        while (sub % 16 == 0 && sub != 0)
+        {
+            bin = String.join("", bin, "11101100", "00010001");
+            sub -= 16;
+        }
+        if (sub % 8 == 0)
+            bin = String.join("", bin, "11101100");
+        return bin;
     }
 }
